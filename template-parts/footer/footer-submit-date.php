5b165356c25c9c4522042fdb9aa57c559a3d65cf
Quonn S Bernard <quonn.bernard@gmail.com>
2022-06-14 09:40:33 -0500

    updated submit date

--- template-parts/footer/footer-submit-date.php
+++ template-parts/footer/footer-submit-date.php
@@ -1,3 +1,3 @@
 <div class="col-12 col-lg-9 col-xl-10 footer__date-submitted fst-italic fw-bolder">
-    <small class="text-uppercase">Submitted: 6-12-2022</small>
+    <small class="text-uppercase">Submitted: 6-14-2022</small>
 </div> 
